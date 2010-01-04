ebf2121948781130790fc950b61acfd47349658e
Alex Kaiser <housenuker@gmail.com>
2010-01-04 13:39:52 -0800

    Modified Settings file to sqlite3. Individual users will likely need to modify the database name.

--- settings.py
+++ settings.py
@@ -9,8 +9,8 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'django.db.backends.sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'C:\Users\Alex\Documents\Inf 191\Dashboard Project\EMGDashboardDB'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
