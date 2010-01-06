e3094e0e785b4c029db211afd410b2ef6ea11057
unknown <Sohrab@.(none)>
2010-01-06 13:52:35 -0800

    Some initial models.
    Decided that left and right column indexes as specified in SD doc made no sense. Moved to xy coords per Widget.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Dashboard(models.Model):
+    #leftColumnWidgets = []
+    #rightColumnWidgets = []
+    sharedWidgets = []
+    lastLogin = models.datetime.now()
+  
+    def add(self, widget, column):
+        self.leftColumnWidgets.append(widget)
+  
+
+class Widget(models.Model):
+    name = models.CharField(max_length=30)
+    belongTo = models.ForeignKey(Dashboard)
+    creator = models.CharField(max_length=50)
+    dateCreated = models.datetime.now()
+    dateUpdated = models.datetime.now()
+    xCoordinates = models.positiveIntegerField()
+    yCoordinates = models.positiveIntegerField()
+    
+
+class Kit(models.Model):
+    name = models.CharField(max_length=30)
+    creator = models.CharField(max_length=50)
+    dateCreated = models.datetime.now()
+    dateUpdated = models.datetime.now()
+    widgetList = [] 
+    
