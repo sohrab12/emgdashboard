9f8a5bb1489a1e9c38975c8345c6d167a37d313d
Alex Kaiser <housenuker@gmail.com>
2010-01-21 14:37:18 -0800

    Working on models and views, trying to get working line graph widget

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,8 +19,14 @@ class Dashboard(WidgetOwner):
 
     #Add a new widget to this dashboard. Y coordinate calculation needs to be written. X coordinate if right needs to be updated
     def addWidget(self, newquery, column):
+        #Create query for the new widget
+        #query = Query(value = "select %s from %s 
         #TODO: Add code to calculate Y-coordinates
-        self.widget_set.create(creator = self.user, query = newquery, x = column, y = 0)
+        self.widget_set.create(creator = self.user, x = column, y = 0)
+
+class Query(models.Model):
+    """A query to be submitted to the database. Each query is referenced as a foreign key by one or more widgets."""
+    value = models.CharField(max_length=200)
 
 class Widget(models.Model):
     """A single sub-display that stores a query to be submitted to the database, and handles the displaying of the resulting data.
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import Image, ImageFont, ImageDraw
 from django.conf import settings
+import DateTime
 
 def graph_view(request):
     #Load the image to be displayed, resize it to the parameters
@@ -10,6 +11,13 @@ def graph_view(request):
         width = int(request.GET['width'])
         height = int(request.GET['height'])
         size = (width, height)
+
+        #Retrieve the left-most and right-most times
+        left-most = DateTime.datetime(request.GET['lefttime'])
+        right-most = DateTime.datetimeP(request.GET['righttime'])
+
+        #Get the model and property to query
+        mode
     except:
         size = im.size
     im = im.resize(size)
