7ec44ffe353339a29035a0b0b02752894a66ff9f
Sohrab Hejazi <sohrab.hejazi@gmail.com>
2010-01-27 00:42:08 -0800

    django htmlTemplate.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
+
+
+
+
 
 urlpatterns = patterns('emgdashboard.dashboard.views',
+    (r'^$', 'index'),
     (r'^graph_chunk$', 'line_graph_view'),
-    (r'^addWidget/$', 'addWidget')
+    (r'^addWidget/$', 'addWidget'),
+
 )
--- urls.py
+++ urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
+
+
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
-    (r'^', include('emgdashboard.dashboard.urls'))
+    (r'^admin/', include('admin.site.urls')),    
+    (r'^dashboard/', include('emgdashboard.dashboard.urls')),     
+    (r'^mymedia/(?P<path>.*)$', 'django.views.static.serve',  
+     {'document_root': settings.MEDIA_ROOT}), 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
 )
