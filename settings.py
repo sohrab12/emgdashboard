7125cbec9146563b55e5607c0e30b92903fe451e
Sam Kaufman <emrysk@gmail.com>
2010-01-29 21:07:55 -0800

    Renamed some directories and cleaned up some files. Also added a HOWTO for making a local_settings module.

--- settings.py
+++ settings.py
@@ -12,22 +12,15 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'EMGDashboardDB.db'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = 'EMGDashboardDB.db'
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = 'America/Los_Angeles'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
 SITE_ID = 1
@@ -38,12 +31,12 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/Users/emrys/Code/emgdashboard/htmlTemplate/'  #absolute path to media
+MEDIA_ROOT = '/Users/emrys/Code/emgdashboard/static/'  #absolute path to media
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/mymedia/'
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -57,7 +50,6 @@ SECRET_KEY = '-h4d@0y&u3^711tgu0p9_b54-8nuc53d-20$%8(3r1je)d0qr4'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -68,9 +60,8 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'emgdashboard.urls'
 
-#TEMPLATE_DIRS = ('C:\Users\Alex\Documents\Inf 191\emgdashboard\htmlTemplate'
 TEMPLATE_DIRS = (
-    "/Users/emrys/Code/emgdashboard/htmlTemplate",
+    "/Users/emrys/Code/emgdashboard/templates",
 )
 
 INSTALLED_APPS = (
--- urls.py
+++ urls.py
@@ -8,9 +8,10 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^admin/', include('admin.site.urls')),    
     (r'^dashboard/', include('emgdashboard.dashboard.urls')),     
-    (r'^mymedia/(?P<path>.*)$', 'django.views.static.serve',  
-     {'document_root': settings.MEDIA_ROOT}), 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
