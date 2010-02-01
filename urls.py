bcbd29d56fd1bfd2905b9d2d08f28f24423095a0
Sohrab Hejazi <sohrab.hejazi@gmail.com>
2010-01-31 19:43:24 -0800

    .

--- urls.py
+++ urls.py
@@ -8,11 +8,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),    
-    (r'^dashboard/', include('emgdashboard.dashboard.urls')),     
+    (r'^dashboard/', include('emgdashboard.dashboard.urls'))    
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
+    urlpatterns += patterns('', (r'^mymedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
