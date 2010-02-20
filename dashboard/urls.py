8758371a2819ca6b801b82fade506dbf5d1fe087
Sam Kaufman <emrysk@gmail.com>
2010-02-19 22:24:48 -0800

    Fixed bug where the admin page would fail because it couldn't find the now-defunct widget_properties view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,11 +7,9 @@ from django.conf import settings
 
 urlpatterns = patterns('emgdashboard.dashboard.views',
     (r'^(?P<dashboard_id>\d+)$', 'index'),
-#    (r'^$', 'index'),
     (r'^graph_chunk/(?P<widget_id>\d+)$', 'line_graph_view'),
     (r'^addWidget/$', 'addWidget'),
     (r'^export_widgets$', 'export_widget'),
     (r'^export_pdf$', 'export_pdf'),
-    (r'^linewidget/(?P<widget_id>\d+)$', 'widget_properties'),
     (r'^slidewidgettimes', 'slide_times'),
     (r'^removewidget$', 'remove_widget'))
